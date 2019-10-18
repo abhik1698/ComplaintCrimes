39b15858c613f00cf96c13cfc94f4fe1317376a4
abhik1698 <abhik1698@gmail.com>
2019-10-18 18:02:48 +0530

    Light edits

--- CriminalHistory.Designer.vb
+++ CriminalHistory.Designer.vb
@@ -40,14 +40,14 @@ Partial Class CriminalHistory
         '
         'uname
         '
-        Me.uname.Location = New System.Drawing.Point(657, 128)
+        Me.uname.Location = New System.Drawing.Point(188, 115)
         Me.uname.Name = "uname"
         Me.uname.Size = New System.Drawing.Size(100, 22)
         Me.uname.TabIndex = 0
         '
         'pname
         '
-        Me.pname.Location = New System.Drawing.Point(657, 175)
+        Me.pname.Location = New System.Drawing.Point(188, 162)
         Me.pname.Name = "pname"
         Me.pname.Size = New System.Drawing.Size(100, 22)
         Me.pname.TabIndex = 1
@@ -57,16 +57,16 @@ Partial Class CriminalHistory
         Me.Button1.BackColor = System.Drawing.SystemColors.Desktop
         Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button1.ForeColor = System.Drawing.SystemColors.HighlightText
-        Me.Button1.Location = New System.Drawing.Point(513, 316)
+        Me.Button1.Location = New System.Drawing.Point(27, 303)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(259, 80)
+        Me.Button1.Size = New System.Drawing.Size(267, 72)
         Me.Button1.TabIndex = 2
         Me.Button1.Text = "Add Inspector"
         Me.Button1.UseVisualStyleBackColor = False
         '
         'rank
         '
-        Me.rank.Location = New System.Drawing.Point(657, 227)
+        Me.rank.Location = New System.Drawing.Point(188, 214)
         Me.rank.Name = "rank"
         Me.rank.Size = New System.Drawing.Size(100, 22)
         Me.rank.TabIndex = 3
@@ -79,15 +79,15 @@ Partial Class CriminalHistory
         Me.mtable.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
         Me.mtable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.mtable.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically
-        Me.mtable.Location = New System.Drawing.Point(778, 96)
+        Me.mtable.Location = New System.Drawing.Point(527, 105)
         Me.mtable.Name = "mtable"
         Me.mtable.RowTemplate.Height = 24
-        Me.mtable.Size = New System.Drawing.Size(586, 384)
+        Me.mtable.Size = New System.Drawing.Size(833, 384)
         Me.mtable.TabIndex = 4
         '
         'pwd
         '
-        Me.pwd.Location = New System.Drawing.Point(657, 269)
+        Me.pwd.Location = New System.Drawing.Point(188, 256)
         Me.pwd.Name = "pwd"
         Me.pwd.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
         Me.pwd.Size = New System.Drawing.Size(100, 22)
@@ -95,7 +95,7 @@ Partial Class CriminalHistory
         '
         'searchText
         '
-        Me.searchText.Location = New System.Drawing.Point(1059, 59)
+        Me.searchText.Location = New System.Drawing.Point(973, 61)
         Me.searchText.Name = "searchText"
         Me.searchText.Size = New System.Drawing.Size(118, 22)
         Me.searchText.TabIndex = 6
@@ -103,7 +103,7 @@ Partial Class CriminalHistory
         'Button2
         '
         Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button2.Location = New System.Drawing.Point(1205, 44)
+        Me.Button2.Location = New System.Drawing.Point(1116, 48)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(110, 37)
         Me.Button2.TabIndex = 7
@@ -114,7 +114,7 @@ Partial Class CriminalHistory
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(806, 59)
+        Me.Label1.Location = New System.Drawing.Point(713, 59)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(237, 24)
         Me.Label1.TabIndex = 8
@@ -125,7 +125,7 @@ Partial Class CriminalHistory
         Me.Label5.AutoSize = True
         Me.Label5.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(491, 128)
+        Me.Label5.Location = New System.Drawing.Point(22, 115)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(117, 25)
         Me.Label5.TabIndex = 12
@@ -136,7 +136,7 @@ Partial Class CriminalHistory
         Me.Label6.AutoSize = True
         Me.Label6.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(491, 269)
+        Me.Label6.Location = New System.Drawing.Point(22, 256)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(113, 25)
         Me.Label6.TabIndex = 13
@@ -147,7 +147,7 @@ Partial Class CriminalHistory
         Me.Label7.AutoSize = True
         Me.Label7.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label7.Location = New System.Drawing.Point(491, 224)
+        Me.Label7.Location = New System.Drawing.Point(22, 211)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(133, 25)
         Me.Label7.TabIndex = 14
@@ -158,7 +158,7 @@ Partial Class CriminalHistory
         Me.Label8.AutoSize = True
         Me.Label8.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label8.Location = New System.Drawing.Point(491, 175)
+        Me.Label8.Location = New System.Drawing.Point(22, 162)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(75, 25)
         Me.Label8.TabIndex = 15
